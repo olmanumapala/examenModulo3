d36fc328fd8c70968ac29f9583f88df3f7e87938
omumana <omumana@mmm.com>
2020-11-06 19:31:58 -0600

    last update

--- api/restassured/src/test/java/BaseTest.java
+++ api/restassured/src/test/java/BaseTest.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
+
+    public RequestSpecification request;
+
+    @BeforeClass
+    @Parameters("baseUrl")
+    public void SetRestAssured(@Optional String baseUrl)
+    {
+        if(baseUrl == null){
+            baseUrl = "https://api-coffee-testing.herokuapp.com";
+        }
+        RestAssured.baseURI = baseUrl;
+        request = RestAssured.given();
+        System.out.println(String.format("Running on %s environment", baseUrl));
+    }
+
+    @BeforeMethod
+    public void Before()
+    {
+        request = RestAssured.given();
+    }
 }
--- api/restassured/src/test/java/RestAssuredTest.java
+++ api/restassured/src/test/java/RestAssuredTest.java
@@ -1,18 +1,70 @@
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
-public class StatusTest extends BaseTest {
+public class RestAssuredTest extends BaseTest {
 
-    private static final String RESOURCE = "/v1/examen/status";
+    private static final String RESOURCESTATUS = "/v1/examen/status";
+    private static final String RESOURCEUPDATE = "/v1/examen/updateName";
+    private static final String RESOURCERANDOM = "/v1/examen/randomName";
+    private static final String RESOURCESAME = "/v1/examen/sameName";
 
     @Test
     public void Status_Test(){
         request
-                .get(String.format("%s", RESOURCE))
+                .get(String.format("%s", RESOURCESTATUS))
                 .then()
                 .statusCode(200)
-                .header("Access-Control-Allow-Origin", equalTo("http://localhost"))
                 .body ("status", equalTo("Listos para el examen"));
+    }
+
+    @Test
+    public void PutName_Test()
+    {
+        request
+                .put(String.format("%s", RESOURCEUPDATE))
+                .then()
+                .statusCode(406)
+                .body ("message", equalTo("Name was not provided"));
+    }
+
+    @Test
+    public void RandomeNameNonAuth_Test()
+    {
+        request
+                .get(String.format("%s", RESOURCERANDOM))
+                .then()
+                .statusCode(401)
+                .body ("message", equalTo("Please login first"));
+    }
+
+    @Test
+    public void RandomeNameAuth_Test()
+    {
+        request
+                .given()
+                .auth()
+                .basic ("testuser", "testpass")
+                .get (String.format("%s", RESOURCERANDOM))
+                .then()
+                .statusCode(200)
+                .body("name", is(not(equalTo(null))));
+    }
+
+    @Test
+    public void PostSameName_Test(){
+
+        JSONObject PostSameName = new JSONObject();
+        PostSameName.put("name", "Edge");
+
+        request
+                .body(PostSameName.toJSONString())
+                .post(String.format("%s", RESOURCESAME))
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Edge"));
     }
 }
